03f5c7d5802b5c6829b0a44eda167199c32ff295
kkeeth <zensin0082@gmail.com>
2023-11-18 00:28:39 +0900

    feat: completed riotjs boilerplate with vitejs

--- src/register-global-components.js
+++ src/register-global-components.js
@@ -2,13 +2,15 @@ import { register } from "riot";
 
 const basename = (path, extension = "") =>
   path.split("/").reverse()[0].replace(extension, "");
-const globalComponentsContext = import.meta.glob([
+const globalComponentsContext = import.meta.glob(
   "./components/global/**/*.riot",
-]);
+  { eager: true }
+);
 
 export default () => {
   Object.entries(globalComponentsContext).map(([path, component]) => {
     const name = basename(path, ".riot");
+
     register(name, component.default || component);
 
     return {
@@ -17,18 +19,3 @@ export default () => {
     };
   });
 };
-
-// export default () => {
-// Object.keys(globalComponentsContext).map((path) => {
-//   const name = basename(path, ".riot");
-
-//   const component = globalComponentsContext(path);
-
-//   register(name, component.default || component);
-
-//   return {
-//     name,
-//     component,
-//   };
-// });
-// };
